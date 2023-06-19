2361edf264214c4f37a42c826c5f4880d588065d
nareshkanna442 <137074188+nareshkanna442@users.noreply.github.com>
2023-06-19 18:50:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.title('My Parents New Helathy Dinner')
 streamlitr.header('Breakfast Menu')
 streamlite.text('Omega 3 & Blueberry Oatmeal')
 streamlite.text('Kale, Spinach & Rocket smoothie')
-  streamlite.text('Hard-boiled Free-range Egg')
+streamit.text('Hard-boiled Free-range Egg')
 
