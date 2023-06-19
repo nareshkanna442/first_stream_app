3c359d257d80aae1f556c3b2e0fdc8689116cf03
nareshkanna442 <137074188+nareshkanna442@users.noreply.github.com>
2023-06-19 18:56:15 +0530

    Update streamlit_app.py
    added breakfast menu 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ import streamlit
 streamlit.title('My Parents New Helathy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞 Avacaddo Toast')
 
+
+ 🥣 🥗 🐔 🥑🍞
